af32a0cc13f2aade1b6e40825dbb47800480e228
Devin Tang <3222561952@qq.com>
2026-02-06 17:08:12 +0800

    feat: 按钮测试

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,53 @@
 </script>
 
 <template>
-  <DeButton type='warning' ref="buttonRef">Button</DeButton>
-  <DeButton plain type="success">Button</DeButton>
-  <DeButton circle type="info">circle Button</DeButton>
-  <DeButton disabled>Button</DeButton>
-  <DeButton round>round Button</DeButton>
+  <section>
+    <div class="row">
+      <DeButton>Default</DeButton>
+      <DeButton type="primary">Primary</DeButton>
+      <DeButton type="success">Success</DeButton>
+      <DeButton type="warning">Warning</DeButton>
+      <DeButton type="danger">Danger</DeButton>
+      <DeButton type="info">Info</DeButton>
+    </div>
+  </section>
+
+  <section>
+    <div class="row">
+      <DeButton plain>Default</DeButton>
+      <DeButton type="primary" plain>Primary</DeButton>
+      <DeButton type="success" plain>Success</DeButton>
+      <DeButton type="warning" plain>Warning</DeButton>
+      <DeButton type="danger" plain>Danger</DeButton>
+      <DeButton type="info" plain>Info</DeButton>
+    </div>
+  </section>
+
+  <section>
+    <div class="row">
+      <DeButton round>Round Button</DeButton>
+      <DeButton type="primary" round>Primary Round</DeButton>
+      <DeButton type="success" circle>√</DeButton>
+      <DeButton type="danger" circle>X</DeButton>
+      <DeButton type="info" circle>i</DeButton>
+    </div>
+  </section>
+
+  <section>
+    <div class="row">
+      <DeButton size="large">Large</DeButton>
+      <DeButton>Default</DeButton>
+      <DeButton size="small">Small</DeButton>
+    </div>
+  </section>
+
+  <section>
+    <div class="row">
+      <DeButton disabled>Disabled Default</DeButton>
+      <DeButton type="primary" disabled>Disabled Primary</DeButton>
+      <DeButton type="success" plain disabled>Disabled Plain Success</DeButton>
+    </div>
+  </section>
 
   <DeCollapse v-model="openedValue" @change="(e) => console.log(e)" accordion>
     <DeCollapseItem name="a" title="Title A">
@@ -29,7 +71,7 @@
     <DeCollapseItem name="c" title="Disabled Title" disabled>
       <div> this is cccc test </div>
     </DeCollapseItem>
-    {{ openedValue }}
+    <!-- {{ openedValue }} -->
   </DeCollapse>
 </template>
 
